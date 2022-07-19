1bb1e6fe3735f6c867125744132f30f03beb9280
Lisa Linux <mclisa00@gmail.com>
2022-07-19 15:42:44 +0100

    Refactoring and working on layout/style

--- store/models.py
+++ store/models.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
+    description = models.TextField(blank=True)
     slug = models.SlugField(max_length=255, unique=True)
 
     class Meta:
@@ -25,7 +26,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='images/')
     slug = models.SlugField(max_length=255)
-    price = models.DecimalField(max_digits=4, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     in_stock = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
