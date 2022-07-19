3c8e9f7bb704ef2a9c135ab0622c56a20d5aa024
Lisa Linux <mclisa00@gmail.com>
2022-07-19 11:34:48 +0100

    Added view for filtering by category

--- store/views.py
+++ store/views.py
@@ -1,10 +1,16 @@
+from nis import cat
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
-def categories(request):
+def categories(request) -> dict:
     return {
         'categories': Category.objects.all()
     }
+
+def category_list(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+    return render(request, 'store/products/category.html', {'category': category, 'products': products})
 
 def all_products(request):
     products = Product.objects.all()
