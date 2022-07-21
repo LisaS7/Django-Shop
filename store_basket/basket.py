25fde88240887b70de7977e6c1535a51f0303ff1
Lisa Linux <mclisa00@gmail.com>
2022-07-21 16:00:29 +0100

    Ajax for add to basket. Saving data to and getting data from session.

--- store_basket/basket.py
+++ store_basket/basket.py
@@ -9,3 +9,13 @@ class Basket:
         if 'session-key' not in self.session:
             basket = self.session['session-key'] = {}
         self.basket = basket
+
+    def add(self, product, quantity):
+        """
+        Adding and updating the session basket data
+        """
+        product_id = product.id
+        if product_id not in self.basket:
+            self.basket[product_id] = {'price': str(product.price), 'quantity': int(quantity)}
+
+        self.session.modified = True
--- store_basket/views.py
+++ store_basket/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
+
+from store.models import Product
+
+from .basket import Basket
 
 
 def basket_summary(request):
     return render(request, 'store/basket/summary.html')
+
+
+def basket_add(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_quantity = int(request.POST.get('quantity'))
+        product = get_object_or_404(Product, id=product_id)
+        
+        basket.add(product=product, quantity=product_quantity)
+        response = JsonResponse({'test': 'data'})
+        return response
