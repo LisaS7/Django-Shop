18a0f4b4c61a8b823d72135ed5fcaf0def1f819f
Lisa Linux <mclisa00@gmail.com>
2022-08-02 13:24:28 +0100

    Reorganised static folder. Additional html tidying. Minor fixes in Store app.

--- store/views.py
+++ store/views.py
@@ -4,7 +4,7 @@ from .models import Category, Product
 
 
 def home(request):
-    return render(request, 'store/index.html')
+    return render(request, 'index.html')
 
 
 def contact(request):
@@ -13,7 +13,7 @@ def contact(request):
 
 def category_list(request, category_slug: str):
     category = get_object_or_404(Category, slug=category_slug)
-    products = Product.objects.filter(category=category)
+    products = Product.products.filter(category=category)
     return render(request, 'store/category.html', {'category': category, 'products': products})
 
 
