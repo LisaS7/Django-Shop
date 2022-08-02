2cdcc23380a7442dbb6f62e8003f887b78b6918c
Lisa Linux <mclisa00@gmail.com>
2022-08-02 10:32:06 +0100

    Moved base template to top level templates folder. Reorganised Store app templates.

--- store/views.py
+++ store/views.py
@@ -4,7 +4,7 @@ from .models import Category, Product
 
 
 def home(request):
-    return render(request, 'store/home.html')
+    return render(request, 'store/index.html')
 
 
 def contact(request):
@@ -14,14 +14,14 @@ def contact(request):
 def category_list(request, category_slug: str):
     category = get_object_or_404(Category, slug=category_slug)
     products = Product.objects.filter(category=category)
-    return render(request, 'store/products/category.html', {'category': category, 'products': products})
+    return render(request, 'store/category.html', {'category': category, 'products': products})
 
 
 def product_all(request):
     products = Product.products.all()
-    return render(request, 'store/products/all.html', {'products': products})
+    return render(request, 'store/all.html', {'products': products})
 
 
 def product_detail(request, slug: str):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, 'store/products/single.html', {'product': product})
+    return render(request, 'store/single.html', {'product': product})
